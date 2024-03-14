10d7b785782dd5f0caa7a1230b079cd397173123
Mauricio Coca <mauricio.coca@bolivia.assuresoft.com>
2024-03-14 15:11:12 -0400

    class ConsoleMuseumWorker created and implements IConsole

--- src/ConsoleVisitor.java
+++ src/ConsoleVisitor.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class ConsoleVisitor implements Console{
+public class ConsoleVisitor implements IConsole {
     private BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
     @Override
