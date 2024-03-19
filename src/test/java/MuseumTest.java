aeb7672082f276e824bd7cba5683219f16ec3aa5
Mauricio Coca <mauricio.coca@bolivia.assuresoft.com>
2024-03-19 14:38:35 -0400

    chore: test folders created

---
+++ src/test/java/MuseumTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MuseumTest {
+
+    @Test
+    public void test1(){
+        assertEquals(true,true);
+    }
+}
